66b30f9539cf13b067caf460630cd1262957ccfc
Daniel Foose <dpfoose@gmail.com>
2020-04-22 21:43:45 -0400

    finding USE embeddings

--- scripts/python/abstract_use_embeddings_postgres.py
+++ scripts/python/abstract_use_embeddings_postgres.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+'''
+Produce CSV files easily readable by Postgres' /copy
+'''
 import gzip
 import json
 import tensorflow_hub as hub
@@ -20,25 +22,38 @@ def load_abstracts(filename):
     return paper_ids, abstracts
 
 
-def embed_abstracts(abstracts):
+def embed_abstracts(abstracts, paper_ids, prefix):
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
-   
+
     print('Loading USE')
     embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
-    
-    def embed_abstract(abstract, i, progress):
+
+    def embed_abstract(abstract, i, paper_id, progress):
         sentences = sent_tokenize(abstract)
         sentence_embeddings = embed(sentences).numpy()
         progress.update(i)
-        return sentence_embeddings.mean(axis=0).to_list()
-    
+        return str(paper_id) + '\t' + '{' + ','.join([str(v) for v in sentence_embeddings.mean(axis=0)]) + '}\n'
+
+    def write_embeddings(embeddings, filename):
+        print(f'writing to {filename}')
+        with open(filename, 'w') as file:
+            file.writelines(embeddings)
+
     print('Computing embeddings')
+    embeddings = []
+    suffix = 0
     with progressbar.ProgressBar(max_value=len(abstracts)) as bar:
-        embeddings = [embed_abstract(abstract, i, bar) for i, abstract in enumerate(abstracts)]
-    return embeddings
+        for i, (paper_id, abstract) in enumerate(zip(paper_ids, abstracts)):
+            if len(embeddings) >= 250000:
+                write_embeddings(embeddings, f'{prefix}_{suffix}.tsv')
+                suffix += 1
+                embeddings = []
+            embeddings.append(embed_abstract(abstract, i, paper_id, bar))
+    suffix += 1
+    write_embeddings(embeddings, f'{prefix}_{suffix}.tsv')
 
 
 def main():
@@ -46,12 +61,7 @@ def main():
     outfilename = sys.argv[2]
     print(f'Loading abstracts from {infilename}')
     paper_ids, abstracts = load_abstracts(infilename)
-    embeddings = embed_abstracts(abstracts)    
-    print(f'Exporting to {outfilename}')
-    if outfilename.endswith('h5'):
-        pd.DataFrame(embeddings, index=paper_ids).to_hdf(outfilename, 'abstract_use_embeddings')
-    else:
-        pd.DataFrame(embeddings, index=paper_ids).to_pickle(outfilename)
+    embed_abstracts(abstracts, paper_ids, outfilename)
 
 
 if __name__ == '__main__':
